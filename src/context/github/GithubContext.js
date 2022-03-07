94cb533b65bb51626bfd224330d7ff6a1693d1e9
Raymond Lopez <raymondblopez97@gmail.com>
2022-03-07 05:51:20 -0500

    add search function

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,23 +16,38 @@ export const GithubProvider = ({ children }) => {
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+
+    const { items } = await response.json();
+
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
 
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
